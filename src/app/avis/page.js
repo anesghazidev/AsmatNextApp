b894e78391146f17420c675aabcdbe12418cb65c
anesghazidev <anes.ghazi.dev@gmail.com>
2026-05-25 15:31:39 +0200

    last commit

--- src/app/avis/page.js
+++ src/app/avis/page.js
@@ -1,6 +1,7 @@
 // DB helper will be imported dynamically inside the component
 import Nav from "@/components/nav";
 import "@/avis.style.css";
+import "@/style.css";
 
 export const dynamic = 'force-dynamic';
 
